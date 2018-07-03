87a432fc7590b225a2f3d8d45ad55e1fbdd52cc3
Carey Jung <carey.jung@gmail.com>
2018-07-03 15:21:13 -0500

    added hyper config files

--- .hyper.js
+++ .hyper.js
@@ -131,7 +131,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["verminal"],
+  plugins: ["verminal", "hyper-solarized-dark"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
