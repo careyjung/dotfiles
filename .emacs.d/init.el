c86b51cdb9d3f40da44487f9f92d3877f70a7d4a
Carey Jung <carey.jung@gmail.com>
2018-07-10 19:19:38 -0500

    set up web-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,6 +42,7 @@
     py-autopep8
     solarized-theme
     exec-path-from-shell
+    web-mode
     ))
 
 (mapc #'(lambda (package)
@@ -78,6 +79,32 @@
   (exec-path-from-shell-initialize))
 ;;(exec-path-from-shell-copy-env "WORKON_HOME")
 ;;(exec-path-from-shell-copy-env "PROJECT_HOME")
+
+;;
+;; web-mode setup
+;;
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+
 
 ;;
 ;; Enable emacs lisp python stuff
