f90c2ef5066f25ec38815951c076015e3f71e8cb
Carey Jung <carey.jung@gmail.com>
2018-08-12 09:31:43 -0500

    revert to default emacs completion styles

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -173,7 +173,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(completion-styles (quote (basic substring)))
  '(create-lockfiles nil)
  '(package-selected-packages
    (quote
